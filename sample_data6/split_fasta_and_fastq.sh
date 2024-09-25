749c8cfd87f10dfee03d767f8d101a1634d26e19
Cyrus Lau <cyruslau.work@gmail.com>
2024-09-25 16:33:33 +0800

    update script

--- sample_data6/split_fasta_and_fastq.sh
+++ sample_data6/split_fasta_and_fastq.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Input FASTA file
+# Input FASTA and FASTQ files
 input_fasta="src/1000Mbp_MP_merged/1000Mbp_MP_merged_m8_SortedBySStart_MSA_degap"
+input_fastq="src/1000Mbp_MP_merged/1000Mbp_MP_merged_m8_SortedBySStart_MSA_degap.fastq"
 
 # Temporary file to store shuffled sequence headers
 temp_headers="temp_headers.txt"
 
-# Shuffle sequence headers
+# Shuffle sequence headers (for pairing purposes)
 grep '^>' "$input_fasta" | shuf > "$temp_headers"
 
-# Function to extract sequences based on headers
-extract_sequences() {
+# Function to extract sequences from FASTA based on headers
+extract_fasta_sequences() {
   awk 'BEGIN{RS=">"; ORS=""} NR==FNR{headers[$1]; next} $1 in headers {print ">"$0}' "$1" "$2"
+}
+
+# Function to extract sequences from FASTQ based on headers
+extract_fastq_sequences() {
+  awk 'NR==FNR{headers[$0]; next} $1 in headers {print; getline; print; getline; print; getline; print}' "$1" "$2"
 }
 
 # Create files of approximate sizes
@@ -20,10 +26,12 @@ for size in "${sizes[@]}"; do
   # Calculate approximate number of sequences needed
   seq_count=$(($(wc -l < "$temp_headers") * size * 1048576 / 143000000))
   
-  # Select random headers and extract sequences
+  # Select random headers and extract sequences for both FASTA and FASTQ
   head -n "$seq_count" "$temp_headers" > "selected_headers.txt"
-  extract_sequences "selected_headers.txt" "$input_fasta" > "output_${size}MB.fasta"
+  extract_fasta_sequences "selected_headers.txt" "$input_fasta" > "output_${size}MB.fasta"
+  sed 's/^>//' "selected_headers.txt" > "selected_headers_no_gt.txt"
+  extract_fastq_sequences "selected_headers_no_gt.txt" "$input_fastq" > "output_${size}MB.fastq"
 done
 
 # Cleanup
-rm "$temp_headers" "selected_headers.txt"
+rm "$temp_headers" "selected_headers.txt" "selected_headers_no_gt.txt"
